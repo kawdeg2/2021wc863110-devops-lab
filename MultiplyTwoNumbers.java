6d737aa47305189ee47642b61c6914f61dc71def
kawdeg2 <kawdeg2@gmail.com>
2025-07-03 19:40:50 +0530

    Create MultiplyTwoNumbers.java

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,15 @@
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        // Declare two integer variables and assign values
+        int number1 = 15;
+        int number2 = 7;
+
+        // Calculate the product
+        int product = number1 * number2;
+
+        // Print the result to the console
+        System.out.println("The product of " + number1 + " and " + number2 + " is: " + product);
+    }
+}
